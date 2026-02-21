000756bd40c792c5ca3b5a6be349c75042aa861d
Rodrigo Galrao <rogalrao5@gmail.com>
2026-02-21 19:34:57 +0000

    started working on Backoffice Added login page and home and login is working

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,8 @@
 <?php
 
 require_once "helpers/base_dados_helper.php";
-require_once "helpers/massagens_helper.php";
+require_once "helpers/colaboradores_helper.php";
 require_once "helpers/extras.php";
+require_once "helpers/massagens_helper.php";
 
 ?>
